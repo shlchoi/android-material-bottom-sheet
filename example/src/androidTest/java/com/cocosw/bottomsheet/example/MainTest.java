01f7d38eaf1704c9f83f52e62794cf4fcdb0627c
Samson Choi <schoi@ifwe.co>
2016-03-01 14:29:43 -0800

    Upgraded support library to v23.2.0

--- example/src/androidTest/java/com/cocosw/bottomsheet/example/MainTest.java
+++ example/src/androidTest/java/com/cocosw/bottomsheet/example/MainTest.java
@@ -1,19 +1,11 @@
 package com.cocosw.bottomsheet.example;
 
-import android.support.test.espresso.Espresso;
 import android.test.ActivityInstrumentationTestCase2;
 
-import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.instanceOf;
 
 public class MainTest extends ActivityInstrumentationTestCase2<Main> {
 
